f363c8cefc5aff4455e3c2fc22e2a17ac5047e4c
Wildan-Byte <cuing453@gmail.com>
2026-02-18 19:55:30 +0700

    Add files via upload

---
+++ Tugas3.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            Toko Pegadaian Syariah
+        </title>
+    </head>
+
+    <body>
+
+        <h2>
+            TOKO PEGADAIAN SYARIAH
+        </h2>
+        <h3>
+            jalan keadilan no 98
+        </h3>
+        <h3>
+            telpon : 089166543377
+        </h3>
+        <h3>
+            program menghitung angsuran hutang
+        </h3>
+
+        <form action="tugas3.1.php" method="POST">
+            Besaran pinjaman:
+            <input type="number" name="pinjaman" value=""> 
+            Lama Angsuran (bulan):
+            <input type="number" name="lama_angsuran" value="">  
+            Keterlambatan (hari):
+            <input type="number" name="terlambat" value=""> 
+
+            <input type="submit" value="kirim">
+        </form>
+
+    </body>
+</html>
